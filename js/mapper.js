6b782a74b04aae78a3607982b240618b60004c3b
Brianna Pritchett <bpritche@mit.edu>
2017-12-05 15:27:21 -0500

    separate recipes from ingredients
    Modified to make recipes visually separate from ingredients by adding
    in a "dummy" chord

--- js/mapper.js
+++ js/mapper.js
@@ -60,19 +60,32 @@ function chordRdr (matrix, mmap) {
       i = d.source.index; j = d.target.index;
       s = _.where(mmap, {id: i });
       t = _.where(mmap, {id: j });
-      m.sname = s[0].name;
+      if (s.length == 0) {
+        m.sname = "dummyNode"; 
+      } else {
+        m.sname = s[0].name;
+      }
       m.sdata = d.source.value;
       m.svalue = +d.source.value;
       m.stotal = _.reduce(matrix[i], function (k, n) { return k + n }, 0);
-      m.tname = t[0].name;
+      if (t.length == 0) {
+        m.tname = "dummyNode";
+      } else {
+        m.tname = t[0].name;
+      }
       m.tdata = d.target.value;
       m.tvalue = +d.target.value;
       m.ttotal = _.reduce(matrix[j], function (k, n) { return k + n }, 0);
     } else {
       g = _.where(mmap, {id: d.index });
-      m.gname = g[0].name;
-      m.gdata = g[0].data;
-      m.gvalue = d.value;
+	  if (g.length == 0) {
+        m.gname = "dummyNode"; m.gdata = "dummyNode"; m.gvalue = d.value;
+      } else {
+        m.gname = g[0].name;
+        m.gdata = g[0].data;
+        m.gvalue = d.value;
+      }
+      
     }
     m.mtotal = _.reduce(matrix, function (m1, n1) { 
       return m1 + _.reduce(n1, function (m2, n2) { return m2 + n2}, 0);
